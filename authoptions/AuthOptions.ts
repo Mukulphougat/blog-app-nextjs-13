2c5b8b4724f3161dddb0d4d7d3c209e28b1f5a9f
Mukul <mukulphoughat@gmail.com>
2023-07-11 14:18:47 +0530

    Add All

--- authoptions/AuthOptions.ts
+++ authoptions/AuthOptions.ts
@@ -11,12 +11,12 @@ const authOptions:NextAuthOptions={
     adapter: PrismaAdapter(client),
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
+            clientId: process.env.GITHUB_ID! as string,
+            clientSecret: process.env.GITHUB_SECRET! as string,
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID as string,
-            clientSecret: process.env.GOOGLE_SECRET as string
+            clientId: process.env.GOOGLE_ID! as string,
+            clientSecret: process.env.GOOGLE_SECRET! as string
         }),
         CredentialsProvider({
             name: "Credentials",
