95979b8b2e89dc722217c6285d48d50b93e32886
Mukul <mukulphoughat@gmail.com>
2023-07-11 14:29:14 +0530

    Add All

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-import {NextRequest, NextResponse} from "next/server";
-
-export default function Middleware(req: NextRequest){
-    // if (req.nextUrl.pathname !== "/login")   return NextResponse.redirect(new URL("/login",req.url))
-}
